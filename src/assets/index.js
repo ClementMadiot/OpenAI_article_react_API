b98a8372b9c53184914af2751071e559e2362de8
ClementMadiot <clementmadiot09@gmail.com>
2024-10-01 14:59:23 +0200

    add form Demo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,19 @@
-import linkIcon from './link.svg'
+
 import loader from './loader.svg'
 import copy from './copy.svg'
 import logo from './logo.svg'
 import tick from './tick.svg'
+import { IoArrowUndo } from "react-icons/io5";
+import { LuLink2 } from "react-icons/lu";
+
+
+
 
 export {
-    linkIcon,
     loader,
     copy,
     logo,
-    tick
+    tick,
+    LuLink2,
+    IoArrowUndo
 }
