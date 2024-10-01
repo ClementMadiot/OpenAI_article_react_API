5e3e7d8eb079df12c4cf230fe2661c29fbd18982
ClementMadiot <clementmadiot09@gmail.com>
2024-10-01 14:28:58 +0200

    Hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,9 +5,11 @@ function Hero() {
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <Nav/>
-      <h1 className="text-5xl font-extrabold leading-[1.15] mt-5 sm:text-6xl text-center">Summarize Article with <br />
+      <h1 className="text-5xl font-extrabold leading-[1.15] mt-5 sm:text-6xl text-center">Summarize Article with <br className="max-md:hidden" />
       <span className="bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent">OpenAI GPT-4</span>
       </h1>
+
+      <h2 className="text-lg sm:text-xl text-center text-gray-700 mt-5 max-w-2xl ">Simplify your reading with Sumnize, an open-source article summarizer that transforms lenghty articles into clear and concise summaries</h2>
     </header>
   );
 }
